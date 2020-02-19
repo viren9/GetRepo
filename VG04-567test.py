cbee168e3ae8f4ecf624dfee88afa4effc30e6b9
viren9 <57375194+viren9@users.noreply.github.com>
2020-02-18 23:51:27 -0500

    Add files via upload

---
+++ VG04-567test.py
@@ -0,0 +1,19 @@
+import unittest
+
+from VG_GetRepo import get_repo_info
+
+class TestGetRepo(unittest.TestCase):
+    def test_normal_response(self):
+        expected = ['User: viren567',
+                    'Repo: 810test Number of commits: 3',
+                    'Repo: 567test Number of commits: 2',
+                    'Repo: Triangle567 Number of commits: 1']
+        self.assertEqual(get_repo_info(), expected)
+
+    def test_bad_user_name(self):
+        self.assertEqual(get_repo_info('qqqwwweee'), 'can not fetch repos from user')
+        self.assertEqual(get_repo_info(''), 'can not fetch repos from user')
+
+
+if _name_ == '_main_':
+    unittest.main()
---
+++ VG_GetRepo.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+
+def get_repo_info(usr_name='viren567'):
+    output = []
+    usr_url = 'https://api.github.com/users/{}/repos'.format(usr_name)
+    res = requests.get(usr_url)
+    repos = json.loads(res.text)
+    output.append('User: {}'.format(usr_name))
+
+    try:
+        repos[0]['name']
+    except (TypeError, KeyError, IndexError):
+        return 'can not fetch repos from user'
+
+    try:
+        for repo in repos:
+            repo_name = repo['name']
+            repo_url = 'https://api.github.com/repos/{}/{}/commits'.format(usr_name, repo_name)
+            repo_if = requests.get(repo_url)
+            repo_if_json = json.loads(repo_if.text)
+            output.append('Repo: {} Number of commits: {}'.format(repo_name, len(repo_if_json)))
+    except (TypeError, KeyError, IndexError):
+        return 'can not fetch commits from repo'
+    return output
+
+
+if _name_ == '_main_':
+    for entry in get_repo_info():
+        print(entry)
